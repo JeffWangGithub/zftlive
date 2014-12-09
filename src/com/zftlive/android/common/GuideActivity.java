2f3565729630787d8954dc0341cfc7c3189b5c78
zengfantian <zftlive@163.com>
2014-12-09 14:27:33 +0800

    移植zxing和共通机能

---
+++ src/com/zftlive/android/common/GuideActivity.java
@@ -0,0 +1,13 @@
+package com.zftlive.android.common;
+
+import com.zftlive.android.base.BaseActivity;
+
+/**
+ * 引导界面
+ * @author 曾繁添
+ * @version 1.0
+ *
+ */
+public class GuideActivity extends BaseActivity {
+
+}
---
+++ src/com/zftlive/android/common/SuggestionActivity.java
@@ -0,0 +1,12 @@
+package com.zftlive.android.common;
+
+import com.zftlive.android.base.BaseActivity;
+
+/**
+ * 意见反馈Activity
+ * @author Administrator
+ * @version 1.0
+ */
+public class SuggestionActivity extends BaseActivity {
+
+}
