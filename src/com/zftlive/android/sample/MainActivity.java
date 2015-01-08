48b035749a2f9aae011796df1776514e0b6800d3
zengfantian <zftlive@163.com>
2015-01-08 09:36:24 +0800

    修改渲染的layout布局文件xml

--- src/com/zftlive/android/sample/MainActivity.java
+++ src/com/zftlive/android/sample/MainActivity.java
@@ -3,25 +3,23 @@ package com.zftlive.android.sample;
 import android.content.Context;
 import android.view.View;
 
+import com.zftlive.android.R;
 import com.zftlive.android.base.BaseActivity;
 
 public class MainActivity extends BaseActivity {
 
 	@Override
 	public int bindLayout() {
-		// TODO Auto-generated method stub
-		return 0;
+		return R.layout.activity_zxing_scanner;
 	}
 
 	@Override
 	public void initView(View view) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void doBusiness(Context mContext) {
-		// TODO Auto-generated method stub
 		
 	}
 
