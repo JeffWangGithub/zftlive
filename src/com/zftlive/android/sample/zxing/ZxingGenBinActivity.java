90cd77a33a906ac7b0cebc65b553d13a052af7b5
zengfantian <zftlive@163.com>
2015-01-27 16:05:28 +0800

    调整代码格式

--- src/com/zftlive/android/sample/zxing/ZxingGenBinActivity.java
+++ src/com/zftlive/android/sample/zxing/ZxingGenBinActivity.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
